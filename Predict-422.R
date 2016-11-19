c22c2a351553985347a9858f672e8e72b3abda6f
Johann Beukes <boerseun@users.noreply.github.com>
2016-11-18 21:43:34 -0500

    Create Predict-422.R
    Initial Commit

---
+++ Predict-422.R
@@ -0,0 +1,29 @@
+########################################
+## PREDICT 422
+## Common Functions
+########################################
+
+
+
+# Example code - Credit Dr. Jennifer Wightman
+separateRFA = function(xData,varName)
+{
+  bytes = c("R","F","A")
+  newVarNames = paste(varName,bytes, sep="_")
+  
+  for (ii in 1:length(bytes)) # Loop over 1 to 3 (corresponding to R, F, and A)
+  {
+    # Find the unique values for current byte
+    byteVals = unique(substr(levels(xData[,varName]),ii,ii))
+    
+    for (jj in 1:length(byteVals)) # Loop over unique byte values
+    {
+      rowIdx = substr(xData[,varName],ii,ii) == byteVals[jj]
+      xData[rowIdx,newVarNames[ii]] = byteVals[jj]
+    }
+    
+    xData[,newVarNames[ii]] = factor(xData[,newVarNames[ii]])
+  }
+  
+  return(xData)
+}
